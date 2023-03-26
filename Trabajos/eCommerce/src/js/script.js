11573cf5ed73d4c3b0022ccc16fe38e4ab8a0ccc
Sebastian-Leal-S <sedastianleal@gmail.com>
2023-03-25 21:25:05 -0500

    Mejora eCommercer

--- Trabajos/eCommerce/src/js/script.js
+++ Trabajos/eCommerce/src/js/script.js
@@ -1,45 +1,19 @@
 $(function () {
-  $('');
-
-  $('#btnProductos').click(function (e) {
-    e.preventDefault();
-    $('#nosotros').hide();
-    $('#carrito').hide();
-    $('#productos').show();
-  });
-
-  $('#btnCarrito').click(function (e) {
-    e.preventDefault();
-    $('#productos').hide();
-    $('#nosotros').hide();
-    $('#carrito').show();
-  });
-
-  $('#btnNosotros').click(function (e) {
-    e.preventDefault();
-    $('#productos').hide();
-    $('#carrito').hide();
-    $('#nosotros').show();
-  });
-
-  const btnPago = $('#btnPagar');
-
+  const btnPagar = $('#btnPagar');
   renderizarListaProductos(productos);
   renderizarTablaCarrito();
+  renderizarTablaCompras();
 
-  btnPago.on('click', function (e) {
+  /* Navegacion */
+  $('#btnsNavbar button').click(function (e) {
     e.preventDefault();
-
-    const idCompra = 'compra_' + new Date().getTime();
-    var carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
-    localStorage.setItem(idCompra, JSON.stringify(carrito));
-
-    localStorage.removeItem('carrito');
-
-    renderizarTablaCarrito();
-    renderizarTablaCompras();
+    const section = $(this).data('section');
+    console.log(section);
+    $('section').hide();
+    $(`#${section}`).show();
   });
 
+  /* Filtra productos por categoria*/
   $('#filtro').on('change', function (e) {
     e.preventDefault();
     const categoria = this.value;
@@ -52,6 +26,46 @@ $(function () {
     }
   });
 
+  /* Evento pago */
+  btnPagar.on('click', function (e) {
+    e.preventDefault();
+
+    let carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
+    /* carrito.push({fecha: new Date().toISOString().substring(0, 10)}); */
+    const idCompra = 'compra_' + new Date().getTime();
+    localStorage.setItem(idCompra, JSON.stringify(carrito));
+
+    localStorage.removeItem('carrito');
+
+    renderizarTablaCarrito();
+    renderizarTablaCompras();
+  });
+
+  /* Agrega los prodcutos al carrito */
+  function agregarProductosCarrito(event, producto) {
+    event.preventDefault();
+
+    const carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
+
+    const form = event.currentTarget;
+    const cantidad = parseInt(form.elements['cantidad'].value);
+    const repetido = carrito.find(prodCarrito => prodCarrito.id === producto.id);
+
+    if (repetido) {
+      repetido.cantidad += cantidad;
+    } else {
+      carrito.push({
+        id: producto.id,
+        name: producto.name,
+        precio: producto.precio,
+        cantidad: cantidad,
+      });
+    }
+
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  }
+
+  /* Renderiza los productos */
   function renderizarListaProductos(arrayProd) {
     const listaProductos = $('#listaProductos ').empty();
 
@@ -69,8 +83,20 @@ $(function () {
             $('<form>', {class: 'comprar'})
               .append(
                 $('<div>', {class: 'input-group'}).append(
-                  $('<input>', {class: 'form-control', type: 'number', name: 'cantidad', value: 1, min: 1}),
-                  $('<input>', {class: 'btn btn-primary', type: 'submit', 'data-id': id, value: 'Agregar al carrito'})
+                  $('<input>', {
+                    class: 'form-control',
+                    type: 'number',
+                    name: 'cantidad',
+                    value: 1,
+                    min: 1,
+                    required: true,
+                  }),
+                  $('<input>', {
+                    class: 'btn btn-primary',
+                    type: 'submit',
+                    'data-id': id,
+                    value: 'Agregar al carrito',
+                  })
                 )
               )
               .on('submit', event => {
@@ -85,59 +111,19 @@ $(function () {
     });
   }
 
-  function agregarProductosCarrito(event, producto) {
-    event.preventDefault();
-
-    const carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
-
-    const form = event.currentTarget;
-    const cantidad = parseInt(form.elements['cantidad'].value);
-    const repetido = carrito.find(prodCarrito => prodCarrito.id === producto.id);
-
-    if (repetido) {
-      repetido.cantidad = cantidad;
-    } else {
-      carrito.push({
-        id: producto.id,
-        name: producto.name,
-        precio: producto.precio,
-        cantidad: cantidad,
-      });
-    }
-
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-  }
-
-  function renderizarTablaCarrito() {
-    const carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
-
-    carrito.length == 0 ? btnPago.prop('disabled', true) : btnPago.prop('disabled', false);
-
-    const tablaCarrito = $('#tablaCarrito').empty();
-
-    carrito.forEach(producto => {
-      const {name, cantidad, precio} = producto;
-
-      tablaCarrito.append(
-        $('<tr>').append(
-          $('<td>').text(name),
-          $('<td>').text(cantidad),
-          $('<td>').text(precio),
-          $('<td>').text(precio * cantidad)
-        )
-      );
-    });
-
-    const total = carrito.reduce((acumulador, producto) => acumulador + producto.precio * producto.cantidad, 0);
-    tablaCarrito.append(
-      $('<tr>').append($('<th>', {scope: 'row', colspan: '3'}).text('Total'), $('<td>').text(`$${total}`))
+  /* Calula el total de la compra */
+  function calcularTotalCompra(compra) {
+    return compra.reduce(
+      (acumulador, producto) => acumulador + producto.precio * producto.cantidad,
+      0
     );
   }
 
-  function renderizarTablaCompras() {
-    //const carrito = JSON.parse(localStorage.getItem("carrito")) ?? []
-
-    const tablaCarrito = $('#tablaCompras').empty();
+  /* Renderiza los el carrito */
+  function renderizarTablaCarrito() {
+    const tablaCarrito = $('#tablaCarrito').empty();
+    const carrito = JSON.parse(localStorage.getItem('carrito')) ?? [];
+    carrito.length == 0 ? btnPagar.prop('disabled', true) : btnPagar.prop('disabled', false);
 
     carrito.forEach(producto => {
       const {name, cantidad, precio} = producto;
@@ -151,5 +137,41 @@ $(function () {
         )
       );
     });
+
+    tablaCarrito.append(
+      $('<tr>').append(
+        $('<th>', {scope: 'row', colspan: '3'}).text('Total'),
+        $('<td>').text(`$${calcularTotalCompra(carrito)}`)
+      )
+    );
+  }
+
+  /* Renderiza tabla de  compras */
+  function renderizarTablaCompras() {
+    const tablaCompras = $('#tablaCompras').empty();
+
+    const comprasSeparadas = Object.entries(localStorage)
+      .filter(([clave, valor]) => clave.startsWith('compra_'))
+      .map(([fecha, productos]) => ({fecha, productos: JSON.parse(productos)}));
+
+    for (const compra of comprasSeparadas) {
+      const filasProductos = compra.productos.map(producto =>
+        $('<tr>').append(
+          $('<td>').text(`${new Date(+compra.fecha.split('_')[1]).toLocaleDateString('es-ES')}`),
+          $('<td>').text(producto.name),
+          $('<td>').text(producto.cantidad),
+          $('<td>').text(producto.precio),
+          $('<td>').text(producto.precio * producto.cantidad)
+        )
+      );
+
+      tablaCompras.append(filasProductos);
+      tablaCompras.append(
+        $('<tr>').append(
+          $('<th>', {scope: 'row', colspan: '4'}).text('Total'),
+          $('<td>').text(`$${calcularTotalCompra(compra.productos)}`)
+        )
+      );
+    }
   }
 });
